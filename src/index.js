76752ceed0372e8148359972406994f47043da9f
ped <peeeedii@gmail.com>
2023-05-08 19:36:33 +0330

    feat: add FA lang

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 
-import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/css/bootstrap.rtl.min.css"
 
 import App from "./App";
 
